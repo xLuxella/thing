74871a96c2f9f05529503cf24a65604f64671a78
xLuxella <76231690+xLuxella@users.noreply.github.com>
2020-12-18 23:52:49 +0000

    Add files via upload

---
+++ i do not know what i am doing.py
@@ -0,0 +1,97 @@
+import time
+#time module
+
+print ("hello")
+time.sleep(1)
+print ("i don't know what i'm doing.")
+time.sleep(1)
+#introduction to code, irrelevant ngl
+
+a = int(input("Please insert your height in centimetres: "))
+#asks user for height input, must be integer as string is invalid.
+
+print ("Your height is "+ str(a) + "cm. ")
+#height output response
+
+if a > 199:
+    print ("JESUS FUCKING CHRIST HOW THE FUCK???")
+elif a > 189:
+    print ("You'd be considered rather tall, if I'm honest.")
+elif a > 179:
+    print ("You're fairly tall, I guess.")
+elif a > 159:
+    print ("Around average height, that's nice.")
+elif a > 149:
+    print ("A bit on the short side I guess.")
+elif a > 139:
+    print ("You're certainly short, if I'm honest.")
+else:
+    print ("Yeah you're tiny, sorry.")
+#additional responses to add on.
+    
+time.sleep(10)
+print ("why are you still here.")
+time.sleep(20)
+print ("ok then. i can wait all day.")
+time.sleep(60)
+print ("can't tell if you're being serious.")
+time.sleep(300)
+print ("five minutes in? pffff. 1435 left.")
+time.sleep(600)
+print ("please tell me you'll leave.")
+time.sleep(750)
+print ("stay for 1 more minute and i'll tell you a fact.")
+time.sleep(810)
+print ("platypuses are 1 of 5 mammals able to lay eggs.")
+time.sleep(830)
+print ("the plural is not platypi. be quiet.")
+time.sleep(900)
+print ("a quarter of an hour in? i'm making toast be back in 5 minutes.")
+time.sleep(1200)
+print ("oh for fucks sake. i think you'll leave after a bit.")
+time.sleep(1800)
+print ("ok since you're not leaving, what the fuck is a wasp.")
+time.sleep(1810)
+print ("they're fucking bees but worse.")
+time.sleep(3600)
+print ("passed the hour mark. 23 left.")
+time.sleep(7200)
+print ("22 hours to go. what exactly are you trying to achieve?")
+time.sleep(10800)
+print ("1 eighth of the way through the day.")
+time.sleep(21600)
+print ("you have been here for 6 hours. maybe you'll actually do this.")
+time.sleep(23456)
+print ("note: its been 23456 seconds. carry on.")
+time.sleep(30000)
+print ("500 minutes in. huh.")
+time.sleep(36000)
+print ("wanna see something cool?")
+time.sleep(36005)
+print ("ok watch this.")
+time.sleep(36010)
+print ("( ͡° ͜ʖ ͡°)")
+time.sleep(43200)
+print ("please go outside.")
+time.sleep(45000)
+print ("don't you have work to do?")
+time.sleep(50000)
+print ("fucking hell.")
+time.sleep(60000)
+print ("oh no.")
+time.sleep(69420)
+print ("it's been 69420 seconds. think you can reach 86400?")
+time.sleep(72000)
+print ("please go outside, please.")
+time.sleep(80000)
+print ("6400 seconds to go... maybe you will make it.")
+time.sleep(86000)
+print ("jesus christ what is wrong with you.")
+time.sleep(86340)
+print ("one more minute. you're FUCKING INSANE.")
+time.sleep(86400)
+print ("ok you win now FUCK OFF AND GO TO SLEEP.")
+time.sleep(172800)
+print ("WHY ARE YOU HERE FOR TWO DAYS GO TO SLEEP.")
+time.sleep(200000)
+print ("www.getalife.com")
